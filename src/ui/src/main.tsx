18971d1636feccaff96985662958b40e919ad2fb
Igor Lacko <iginonajlepsi@gmail.com>
2025-08-25 16:13:55 +0200

    fix(packages): removed unused packages

--- src/ui/src/main.tsx
+++ src/ui/src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client';
 import { ReactNode } from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import 'typeface-lobster';
 import '@fontsource/roboto';
 
 import './index.css';
